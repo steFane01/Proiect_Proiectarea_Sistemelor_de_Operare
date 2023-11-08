82b64da94600cbd4f90038057258bcded02cee40
Seby4629 <119496075+Seby4629@users.noreply.github.com>
2023-11-08 15:29:58 +0200

    Update CreatingFile.c

--- CreatingFile.c
+++ CreatingFile.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    // Verifică numărul de argumente
     if (argc != 2) {
         printf("Utilizare: %s nume_fisier\n", argv[0]);
         return 1;
     }
 
-    const char *filename = argv[1];
-    FILE *file = fopen(filename, "w");
+    const char *partitionPath = "/dev/sda4";
+    const char *filename = argv[1]; 
 
-    if (file == NULL) {
-        perror("Eroare la deschiderea fișierului.");
+    int partition = open(partitionPath, O_CREAT | O_WRONLY, 0666);
+
+    if (partition == -1) {
+        perror("Eroare la deschiderea partiției.");
         return 1;
     }
 
     const char *data = "Acesta este conținutul fișierului.\n";
     size_t dataSize = strlen(data);
 
-    // Scrie datele în fișier
-    size_t bytesWritten = fwrite(data, 1, dataSize, file);
+    ssize_t bytesWritten = write(partition, data, dataSize);
 
-    if (bytesWritten != dataSize) {
-        perror("Eroare la scrierea în fișier.");
-        fclose(file);
+    if (bytesWritten == -1) {
+        perror("Eroare la scrierea în partiție.");
+        close(partition);
         return 1;
     }
 
-    printf("S-a scris %zd bytes în fișierul %s.\n", bytesWritten, filename);
+    printf("S-a scris %zd bytes în partiție.\n", bytesWritten);
 
-    fclose(file);
+    close(partition);
 
     return 0;
 }
